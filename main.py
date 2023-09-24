2a5345eb184fa07d4cbe7dfe33546a33646ba642
philjoong <91088992+philjoong@users.noreply.github.com>
2023-09-24 18:17:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,68 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 
+import os
+import tempfile
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
+# from PyPDF2 import PdfMerger
 
-st.title("L2M 업데이트 검색")
-title = st.text_input('업데이트 관련 질문해주세요', '가장 최근 업데이트 정보를 알려줘')
+# loader = PyPDFLoader("output.pdf")
+# pages = loader.load_and_split()
 
-if st.button('검색'):
-    with st.spinner('Wait for it...'):
-            st.write('질문 내용: ', title)
-    st.success('Done!')
+st.title("pdf 검색")
+st.write("---")
 
-# chat_model = ChatOpenAI()
+def pdf_to_document(uploaded_file):
+    # Read documents
+    temp_dir = tempfile.TemporaryDirectory()
+    for file in uploaded_file:
+        temp_filepath = os.path.join(temp_dir.name, file.name)
+        with open(temp_filepath, "wb") as f:
+            f.write(file.getvalue())
+        loader = PyPDFLoader(temp_filepath)
+        pages = loader.load_and_split()
+        return pages
+    
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 400,
+        chunk_overlap  = 20,
+        length_function = len,
+        is_separator_regex = False,
+    )
+    texts = text_splitter.split_documents(pages)
 
-# result = chat_model.predict("hi!")
+    embeddings_model = OpenAIEmbeddings()
+    # load it into Chroma
+    db = Chroma.from_documents(texts, embeddings_model)
 
-# print(result)
+    question = st.text_input('pdf 관련 질문해주세요', '입력 창')
+
+    if st.button('검색'):
+        with st.spinner('Wait for it...'):
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+                qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+                result = qa_chain({"query": question})
+                st.write('질문 내용: ', question)
+                st.write(result["result"])
+
+        st.success('Done!')
+
+
+
+# merger = PdfMerger()
+# pdf_dir = os.getcwd()
+# for filename in os.listdir(pdf_dir):
+#     if filename.endswith('.pdf'):
+#          merger.append(filename)
+# merger.write("output.pdf")
+# merger.close()
